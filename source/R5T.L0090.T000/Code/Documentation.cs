389d5ebfc98ec4ad94c5346653a1dfbbf4f1ee0e
davidcoats <david@getsafetycone.com>
2024-01-29 18:34:13 -0800

    Interim changes.

--- source/R5T.L0090.T000/Code/Documentation.cs
+++ source/R5T.L0090.T000/Code/Documentation.cs
@@ -1,10 +1,10 @@
 using System;
 
 
-namespace R5T.L0090
+namespace R5T.L0090.T000
 {
 	/// <summary>
-	/// Simplified IEqualityComparer&lt;T&gt; library.
+	/// Simplified IEqualityComparer&lt;T&gt; types library.
 	/// </summary>
 	public static class Documentation
 	{
---
+++ source/R5T.L0090.T000/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.L0090.T000
+Simplified IEqualityComparer<T> types library.
